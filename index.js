85f25e0b315b36615a88129ed884585f880bb1fb
iribae <xiphion.iris@gmail.com>
2017-10-18 00:09:39 +0200

    cleaned up
    someone test it tho

--- index.js
+++ index.js
@@ -1,131 +1,50 @@
 const Command = require('Command')
 const StateTracker = require('tera-state-tracker')
-
+const bonfireid = {
+	'red': 1,
+	'fire' : 2,
+	'santa' : 6,
+	'blue' : 8,
+	'purple' : 9,
+	'fos' : 10
+}
+var cidnum = [6969]
 module.exports = function bonfire(dispatch) {
 	const command = Command(dispatch)
 	const state = StateTracker(dispatch)
 	
-command.add('purple', () => {
-	command.message('Spawning purple bonfire.')
-	dispatch.toClient('S_SPAWN_BONFIRE', 1, {
-	unk1: 0,
-	cid: 6969696969,
-	type: 9, 
-	unk2: 0,
-	x: state.client.position.x + 30,
-	y: state.client.position.y + 30,
-	z: state.client.position.z + 30,
-	state: 0 
+	command.add('bonfire', (arg) => {
+		command.message('Spawning ' + arg + ' bonfire.')
+		dispatch.toClient('S_SPAWN_BONFIRE', 1, {
+			unk1: 0,
+			cid: cidnum[cidnum.length-1]+1,
+			type: bonfireid[arg], 
+			unk2: 0,
+			x: state.client.position.x + 30,
+			y: state.client.position.y + 30,
+			z: state.client.position.z + 30,
+			state: 0 
 		})
+		cidnum.push(cidnum[cidnum.length-1]+1)
 	})
-	command.add('red', () => {
-	command.message('Spawning red bonfire.')
-	dispatch.toClient('S_SPAWN_BONFIRE', 1, {
-	unk1: 0,
-	cid: 6969696970,
-	type: 1, // 
-	unk2: 0,
-	x: state.client.position.x + 30,
-	y: state.client.position.y + 30,
-	z: state.client.position.z + 30,
-	state: 0 
-		})
-	})
-	command.add('blue', () => {
-	command.message('Spawning blue bonfire.')
-	dispatch.toClient('S_SPAWN_BONFIRE', 1, {
-	unk1: 0,
-	cid: 6969696971,
-	type: 8, 
-	unk2: 0,
-	x: state.client.position.x + 30,
-	y: state.client.position.y + 30,
-	z: state.client.position.z + 30,
-	state: 0 
-		})
-	})
-	command.add('santa', () => {
-	command.message('Spawning santa candle.')
-	dispatch.toClient('S_SPAWN_BONFIRE', 1, {
-	unk1: 0,
-	cid: 6969696972,
-	type: 6, 
-	unk2: 0,
-	x: state.client.position.x + 30,
-	y: state.client.position.y + 30,
-	z: state.client.position.z + 30,
-	state: 0 
-		})
-	})
-	command.add('fos', () => {
-	command.message('Spawning flame of sacrifice.')
-	dispatch.toClient('S_SPAWN_BONFIRE', 1, {
-	unk1: 0,
-	cid: 6969696973,
-	type: 10, 
-	unk2: 0,
-	x: state.client.position.x + 30,
-	y: state.client.position.y + 30,
-	z: state.client.position.z + 30,
-	state: 0 
-		})
-	})
-	command.add('fire', () => {
-	command.message('Spawning bonfire without stand.')
-	dispatch.toClient('S_SPAWN_BONFIRE', 1, {
-	unk1: 0,
-	cid: 6969696974,
-	type: 2, 
-	unk2: 0,
-	x: state.client.position.x + 30,
-	y: state.client.position.y + 30,
-	z: state.client.position.z + 30,
-	state: 0 
-		})
-	})
-	command.add('despawnred', () => {
-		command.message('Despawning red bonfire.')
+	
+	command.add('despawnlast', () => {
+		command.message('Despawning last bonfire.')
 		dispatch.toClient('S_DESPAWN_BONFIRE', 1, {
-		cid: 6969696970,
-		unk: 0
+			cid: cidnum[cidnum.length-1],
+			unk: 0
 		})
+		cidnum.pop(cidnum.length-1)
 	})
-	command.add('despawnblue', () => {
-		command.message('Despawning blue bonfire.')
-		dispatch.toClient('S_DESPAWN_BONFIRE', 1, {
-		cid: 6969696971,
-		unk: 0
-		})
-	})
-	command.add('despawnpurple', () => {
-		command.message('Depsawning purple bonfire.')
-		dispatch.toClient('S_DESPAWN_BONFIRE', 1, {
-		cid: 6969696969,
-		unk: 0
-		})
-	})
-	command.add('despawnsanta', () => {
-		command.message('Despawning santa candle.')
-		dispatch.toClient('S_DESPAWN_BONFIRE', 1, {
-		cid: 6969696972,
-		unk: 0
-		})
-	})
-	command.add('despawnfire', () => {
-		command.message('Despawning fire without stand.')
-		dispatch.toClient('S_DESPAWN_BONFIRE', 1, {
-		cid: 6969696974,
-		unk: 0
-		})
-	})
-	command.add('despawnfos', () => {
-		command.message('Despawning flame of sacrifice.')
-		dispatch.toClient('S_DESPAWN_BONFIRE', 1, {
-		cid: 6969696973,
-		unk: 0
-		})
+	command.add('despawnall', () => {
+		command.message('Despawning all bonfires.')
+		for (let removekebab of bonfireid)
+		{
+			dispatch.toClient('S_DESPAWN_BONFIRE', 1, {
+				cid: removekebab,
+				unk: 0
+			})
+			bonfireid.splice(bonfireid.indexOf(removekebab))
+		}
 	})
 }
-
-
-	
